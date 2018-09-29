69d20b05a6b2772ee6a993da5272953d699544ad
Anderson do Carmo Nunes <anderson.nunes17@etec.sp.gov.br>
2018-09-29 20:36:56 -0300

    Adicionando cursos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,37 @@
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { Router, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos/cursos.service';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent,
+    CabecalhoComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/curso-detalhe/curso-detalhe.component.ts
@@ -0,0 +1,45 @@
+import { CursosService } from './../cursos/cursos.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+@Component({
+  selector: 'app-curso-detalhe',
+  templateUrl: './curso-detalhe.component.html',
+  styleUrls: ['./curso-detalhe.component.css']
+})
+export class CursoDetalheComponent implements OnInit {
+
+  id: number;
+  inscricao: Subscription;
+  curso: any;
+
+  //contrutor para trabalhar com a rota ativa
+  constructor(
+    private route: ActivatedRoute,
+    private cursosService: CursosService,
+    private router: Router
+    ) { 
+    //this.id = this.route.snapshot.params['id'];
+  }
+
+  ngOnInit() {
+    //obter o ID dinamicamente escutando o id
+    this.inscricao = this.route.params.subscribe(
+      (params: any) => {
+        this.id = params['id'];
+        this.curso = this.cursosService.getCurso(this.id);
+        
+        //redirecionando se o id nao estiver cadastrado
+        if(this.curso == null){
+            this.router.navigate(['/naoEncontrado']);
+        }
+      }
+    );
+  }
+  ngOnDestroy(){
+    //remover inscrição quando o componente for destruido
+    this.inscricao.unsubscribe();
+  }
+
+}
