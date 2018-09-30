1c0e88c4daedb11f0e8eca64188579ea956a2ada
Anderson do Carmo Nunes <anderson.nunes17@etec.sp.gov.br>
2018-09-29 21:15:29 -0300

    Mudança de Página Teste

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,3 +1,5 @@
+import { Subscription } from 'rxjs/Subscription';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CursosService } from './cursos.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -9,11 +11,37 @@ import { Component, OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   cursos: any[];
+  pagina: number;
+  inscricao: Subscription;
 
-  constructor(private cursosService: CursosService) { }
+  constructor(
+    private cursosService: CursosService,
+
+    //pega a rota atual
+    private route: ActivatedRoute,
+
+    private router: Router
+    ) { }
 
   ngOnInit() {
     this.cursos = this.cursosService.getCursos();
+
+    //pegando o parametros de query
+    this.inscricao = this.route.queryParams.subscribe(
+      (queryParams: any) =>{
+        this.pagina = queryParams['pagina'];
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.inscricao.unsubscribe();
+  }
+  proximaPagina(){
+    this.pagina++;
+    //botao mudando de pagina
+    this.router.navigate(['/cursos'],
+    {queryParams: {pagina: this.pagina}});
   }
   
 
